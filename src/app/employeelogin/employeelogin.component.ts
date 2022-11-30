aac65402ea573b9c5198f25f4b1b8478faa31809
goodwinjose26 <josegoodwin22@gmail.com>
2022-11-30 16:11:49 +0530

    employeelogin

---
+++ src/app/employeelogin/employeelogin.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-employeelogin',
+  templateUrl: './employeelogin.component.html',
+  styleUrls: ['./employeelogin.component.css']
+})
+export class EmployeeloginComponent {
+
+  id=""
+  password=""
+  constructor(private route:Router){}
+  adminLoginvalues =()=>
+  {
+    let data:any =
+    {
+      "id":this.id,"password":this.password
+    }
+    console.log(data)
+    if (this.id =="1122" && this.password=="12345") {
+  
+this.route.navigate(["/viewcourhse"])      
+
+    } else {
+      alert("invalid credentials")
+      
+    }
+  }
+
+}
