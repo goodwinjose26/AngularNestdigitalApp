ea27f68dded69854bb85a13b997f05a919bcabd6
goodwinjose26 <josegoodwin22@gmail.com>
2022-11-30 12:42:15 +0530

    homecontent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomenavbarComponent } from './homenavbar/homenavbar.component';
+import { HomecontentComponent } from './homecontent/homecontent.component';
+import { RouterModule, Routes } from '@angular/router';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:HomecontentComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomenavbarComponent
+    HomenavbarComponent,
+    HomecontentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
